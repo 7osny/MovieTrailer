fcf36c3f72c0b4735cb792c7b4057317d692bdc1
Mohammed Hosny <mohammedhosny94@hotmail.com>
2017-03-15 07:05:30 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# NanoDegreeFirstProject
+# Project: Udacity:Programming Foundations with Python:MovieTrailer
+# Movie Trailer 
+
+A Movie Trailer Website where users can see my favorite movies , read the short story about movie and watch the trailers.
+
+ 
+# Quick Start
+
+  
+1- Download the code
+ 
+2- Install python 2.7
+3- Go to cmd and write this command  python entertainment.py <br>
+NOTE:<br>
+you must add python to environment variable to run the pervious command from cmd
+
+
+
+# What's included
+
+Excluding this `README.md`
+ file there are three python files:
+
+ 
+ - **entertainment_center.py** 
+- The main entry to the program. Builds the movie list and calls the method to generate
+  
+ the web page.
+  - **fresh_tomatoes.py** 
+- The provided web page generator.
+ It takes in a list of movies and builds the `HTML`
+ and opens up
+  your default browser to display the generated `HTML` page.
+
+  - **media.py** - Contains the class that provides the data structure to hold movie information.
+
+  
+A fourth file, `fresh_tomatoes.html`, will be generated when you run the program as described above.
+ 
+This file is the actual web page you would see in your browser.
+
+#
---
+++ entertainment_center.py
@@ -0,0 +1,178 @@
+import media
+import fresh_tomatoes
+# Create instances of the Movie class to hold information
+
+# first movie
+
+The_Shawshank_Redemption = media.Movie(
+        "The Shawshank Redemption",
+        "Two imprisoned men bond over a number of years, finding solace and "
+        "eventual redemption through acts of common decency.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU"
+        "4NjIwNl5BMl5Ban"
+        "BnXkFtZTgwMDU2MjEyMDE@._V1_SY1000_CR0,0,672,1000_AL_.jpg",
+        "https://www.youtube.com/watch?v=NmzuHjWmXOc"
+        )
+
+# second movie
+
+The_Green_Mile = media.Movie(
+        "The Green Mile",
+        "The lives of guards on Death Row are affected by one of their"
+        "charges: a black man accused of child murder and rape, yet who"
+        "has a mysterious gift.",
+        "https://images-na.ssl-images-amazon.com/images/M/"
+        "MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_.jpg",
+        "https://www.youtube.com/watch?v=Ki4haFrqSrw"
+        )
+
+# third movie
+
+Fight_club = media.Movie(
+        "Fight Club",
+        "An insomniac office worker, looking for a way to change"
+        "his life, crosses paths with a devil-may-care soap maker,"
+        "forming an underground fight club that evolves into something"
+        "much, much more.",
+        "https://images-na.ssl-images-amazon.com/images/M/"
+        "MV5BZGY5Y2RjMmItNDg5Yy00NjUwLThjMTEtNDc2OGUzNTBiYm"
+        "M1XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+        "https://www.youtube.com/watch?v=NjkWLP24LkU"
+        )
+
+# fourth movie
+
+Interstellar = media.Movie(
+         "Interstellar ",
+         "A team of explorers travel through a wormhole in space in an attempt"
+         "to ensure humanity's survival.",
+         "https://images-na.ssl-images-amazon.com/images"
+         "/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@."
+         "_V1_SY1000_CR0,0,640,1000_AL_.jpg",
+         "https://www.youtube.com/watch?v=oKNy-MWjkcU"
+         )
+
+# fifth movie
+
+Lord_of_the_Rings = media.Movie(
+         "The Lord of the Rings",
+         "Gandalf and Aragorn lead the World of Men against"
+         "Sauron's army to draw his gaze from Frodo"
+         "and Sam as they approach Mount Doom with the One Ring.",
+         "http://fontmeme.com/images/The-Lord-of-the-Rings-Poster.jpg",
+         "https://www.youtube.com/watch?v=V75dMMIW2B4"
+        )
+
+# sixth movie
+
+Inception = media.Movie(
+         "Inception ",
+         "A thief, who steals corporate secrets through use of"
+         "dream-sharing technology, is given the inverse task of"
+         "planting an idea into the mind of a CEO",
+         "https://images-na.ssl-images-amazon.com/images"
+         "/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@."
+         "_V1_SY1000_CR0,0,675,1000_AL_.jpg",
+         "https://www.youtube.com/watch?v=8hP9D6kZseM"
+         )
+
+# seven movie
+
+Eternal_Sunshine_of_the_Spotless_Mind = media.Movie(
+        "Eternal Sunshine of the Spotless Mind",
+        "When their relationship turns sour, a couple undergoes a procedure "
+        "to have each other erased from their memories. But it is only through"
+        "the process of loss "
+        "that they discover what they had to begin with.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY4NzcwOD"
+        "g3Nl5BMl5BanBnX"
+        "kFtZTcwNTEwOTMyMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+        "https://www.youtube.com/watch?v=0zFywiAh7N0"
+        )
+
+# eighth movie
+
+The_Dark_Knight = media.Movie(
+         "The Dark Knight",
+         "When the menace known as the Joker wreaks havoc and chaos"
+         "on the people of Gotham, the caped crusader must come to "
+         "terms with one of the greatest"
+         "psychological tests of his ability to fight injustice.",
+         "https://images-na.ssl-images-amazon.com/images"
+         "/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@."
+         "_V1_SY1000_CR0,0,675,1000_AL_.jpg",
+         "https://www.youtube.com/watch?v=yrJL5JxEYIw"
+         )
+
+# ninth movie
+
+Requiem_for_aDream = media.Movie(
+        "Requiem for a Dream",
+        "The drug-induced utopias of four Coney Island people"
+        " are shattered when"
+        "their addictions run deep.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BO"
+        "TdiNzJlOWUtNWMwNS00NmFlLWI0YTEt"
+        "ZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000"
+        "_CR0,0,666,1000_AL_.jpg",
+        "https://www.youtube.com/watch?v=MD_0v1snGVo"
+        )
+
+# tenth movie
+
+Collateral_Beauty = media.Movie(
+         "Collateral Beauty",
+         "Retreating from life after a tragedy, a man questions"
+         "the universe by writing to Love, Time and Death."
+         "Receiving unexpected answers, he begins to see how these things"
+         "interlock and how even loss can reveal"
+         " moments of meaning and beauty.",
+         "http://sodasandpopcorn.com/blog/wp-content"
+         "/uploads/2017/01/colla.jpg",
+         "https://www.youtube.com/watch?v=isQ5Ycie73U"
+         )
+
+# eleventh movie
+
+La_la_Land = media.Movie(
+         "La la Land",
+         " American romantic musical comedy-drama film",
+         "http://schoolpress.cdn.whipplehill.net/gcschool648"
+         "/4/files/2017/01/image1.jpeg",
+         "https://www.youtube.com/watch?v=0pdqf4P9MB8")
+
+# twelfth movie
+
+Sully = media.Movie(
+         "Sully",
+         "A movie about The story of Chesley Sullenberger,"
+         "an American pilot who became a hero after landing"
+         "his damaged plane on the Hudson River in order to"
+         "save the flight's passengers and crew.",
+         "http://www.telegraph.co.uk/content/dam/films/2016/11"
+         "/02/sully-poster-xlarge_trans_NvBQzQNjv4Bq1V8_3oXt_"
+         "XBWwkgI1jrKEdIUNQoSuawlNHgUMA8NwmE.jpg",
+         "https://www.youtube.com/watch?v=JZ3zSRgDtkA"
+         )
+
+
+# Add the instances to a list
+
+movies = [
+    The_Shawshank_Redemption,
+    The_Green_Mile,
+    Fight_club,
+    Interstellar,
+    Lord_of_the_Rings,
+    Inception,
+    The_Dark_Knight,
+    Requiem_for_aDream,
+    Collateral_Beauty,
+    La_la_Land,
+    Sully,
+    Eternal_Sunshine_of_the_Spotless_Mind
+]
+
+# Generate a web page that displays the movies in the list
+
+fresh_tomatoes.open_movies_page(movies)
